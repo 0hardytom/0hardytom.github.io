dffd672f41bf0bc413eba351408ca12c5aa83948
0hardytom <138515389+0hardytom@users.noreply.github.com>
2025-09-14 22:48:33 +0000

    Deploying to gh-pages from @ 0hardytom/0hardytom.github.io@644791ca3adaf0ee2f720f6fc5e26d08f057fa8d 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "0hardytom/website",
+    "data-repo": "0hardytom/0hardytom.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
